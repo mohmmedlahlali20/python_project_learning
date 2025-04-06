9ef9b415073828d96c4600f0c60ee823e3e32754
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-04-06 11:50:42 +0100

    chore: update config settings

--- config/config.py
+++ config/config.py
@@ -10,3 +10,6 @@ class Config:
     MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
     MYSQL_DB = os.getenv('MYSQL_DB')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16MB max
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
