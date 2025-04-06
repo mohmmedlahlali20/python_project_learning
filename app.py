8adbcffb874b8e88f5e210a223d6724bbf2c4e48
mohmmedlahlali20 <mohmmedlaeh81@gmail.com>
2025-04-06 11:50:41 +0100

    refactor: update app entrypoint

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_mysqldb import MySQL
 from config.config import Config
 from auth.routes import auth_bp
+from products.routes import product_bp
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +11,8 @@ app.config.from_object(Config)
 mysql = MySQL(app)
 
 app.register_blueprint(auth_bp)
+app.register_blueprint(product_bp)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
